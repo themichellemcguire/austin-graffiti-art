8297f772ee5659606b0eb1c57902464028b533b5
owner <themichellemcguire@gmail.com>
2019-04-15 23:37:01 -0500

    update api, routes, directories

--- src/components/config.js
+++ src/components/config.js
@@ -2,3 +2,6 @@ exports.CLIENT_ORIGIN = process.env.NODE_ENV === 'production'
   ? 'https://austin-graffiti-art.herokuapp.com/'
   : 'http://localhost:3000'
 
+  export const API_URL = process.env.NODE_ENV === 'production'
+  ? 'https://austin-graffiti-art.herokuapp.com/'
+  : 'http://localhost:3001'
